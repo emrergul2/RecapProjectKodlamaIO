1fb9c018ddc72a210ea3fa6697f1119932d11adb
emrergul2 <emrergul2@gmail.com>
2022-05-22 19:38:37 +0300

    lecture-11 => IOC

---
+++ RecapProject.DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RecapProject.Core.DataAccess.EntityFramework;
+using RecapProject.DataAccess.Abstract;
+using RecapProject.Entities.Concrete;
+
+namespace RecapProject.DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, RecapProjectContext>, IRentalDal
+    {
+
+    }
+}
